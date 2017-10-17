cf276d6fe273671ab47aba9a5d7e8fbcebaa0d95
Gustavo Giroldo <gustavoglu@hotmail.com>
2017-10-16 23:21:11 -0200

    Corrigindo problemas com partial views

--- src/Comerciante.Pedido.Presentation.Site/wwwroot/js/referencias.js
+++ src/Comerciante.Pedido.Presentation.Site/wwwroot/js/referencias.js
@@ -1,5 +1,7 @@
 ﻿
+ko.virtualElements.allowedBindings.foreachprop = true;
 var ViewModel = new viewModel();
+ko.applyBindings(ViewModel);
 
 $(document).ready(function () {
 
@@ -8,41 +10,105 @@ $(document).ready(function () {
     for (var i = 0; i < modelJS.length; i++) {
 
         var model = modelJS[i];
-        var imagemA = model.referencia.referencia_Imagens[0].Uri;
-        var imagemB = model.referencia.referencia_Imagens[1].Uri;
-        var Referencia = new referencia(model.referencia.Codigo, model.referencia.Descricao, model.referencia.Preco, imagemA, imagemB);
-        ViewModel.listReferencias.push(model);
+
+        var cores = model.cores;
+        var tamanhos = ViewModel.criaTamanhos(model.tamanhos);
+        var imagemA = model.referencia.referencia_Imagens[0].uri;
+        var imagemB = model.referencia.referencia_Imagens[1].uri;
+        var campos = ViewModel.criaCampos(cores, tamanhos);
+        var Referencia = new ViewModel.referencia(model.referencia.codigo, model.referencia.descricao, model.referencia.preco, imagemA, imagemB, tamanhos, cores, campos);
+
+        ViewModel.listReferencias.push(Referencia);
     }
-
-    ko.applyBindings(ViewModel);
-
 });
 
 
 function viewModel() {
 
     var self = this;
+    self.selectedRow = ko.observable();
     self.listReferencias = ko.observableArray();
-}
 
-function referencia(codigo,descricao,preco,imagemA,imagemB) {
+    self.AddAoPedido = function (row) {
 
-    var self = this;
-    self.Codigo = ko.observable(codigo);
-    self.Descricao = ko.observable(descricao);
-    self.Preco = ko.observable(preco);
-    self.ImagemA = ko.observable(imagemA);
-    self.ImagemB = ko.observable(imagemB);
-}
+        ViewModel.selectedRow(row);
+        var model = ko.toJS(row);
+
+        self.addEditRef.Codigo(model.Codigo);
+        self.addEditRef.Descricao(model.Descricao);
+        self.addEditRef.Preco(model.Preco);
+        self.addEditRef.ImagemA(model.ImagemA);
+        self.addEditRef.ImagemB(model.ImagemB);
+        self.addEditRef.Tamanhos(model.Tamanhos);
+        self.addEditRef.Cores(model.Cores);
+        self.addEditRef.Campos(model.Campos);
+
+        $('#modalAddEditRef').modal('show');
+
+    }
+
+    self.addEditRef = {
+
+        Codigo: ko.observable(),
+        Descricao: ko.observable(),
+        Preco: ko.observable(),
+        ImagemA: ko.observable(),
+        ImagemB: ko.observable(),
+        Tamanhos: ko.observable(),
+        Cores: ko.observable(),
+        Campos: ko.observable()
+    }
 
 
-$('.btAdd').click(btAdd);
+    self.campo = function (cor, tamanho, quantidade) {
 
+        var self = this;
+        self.Cor = ko.observable(cor);
+        self.Tamanho = ko.observable(tamanho);
+        self.Quantidade = ko.observable(quantidade);
+    }
 
-function btAdd() {
+    self.referencia = function (codigo, descricao, preco, imagemA, imagemB, tamanhos, cores, campos) {
 
-    var modelSer = 
+        var self = this;
+        self.Codigo = ko.observable(codigo);
+        self.Descricao = ko.observable(descricao);
+        self.Preco = ko.observable(preco);
+        self.ImagemA = ko.observable(imagemA);
+        self.ImagemB = ko.observable(imagemB);
+        self.Tamanhos = tamanhos;
+        self.Cores = cores;
+        self.Campos = ko.observableArray(campos);
+    }
 
-    $('#modalAddEditRef').modal("show");
+    self.criaCampos = function (cores, tamanhos) {
 
+        var campos = [];
+
+        for (var i = 0; i < cores.length; i++) {
+            var cor = cores[i];
+
+            for (var j = 0; j < tamanhos.length; j++) {
+
+                var tamanho = tamanhos[j];
+                var campo = new self.campo(cor, tamanho, 0);
+                campos.push(campo);
+            }
+        }
+
+        return campos;
+    }
+
+    self.criaTamanhos = function (tamanhos) {
+
+        var tamanhoNull = { id: null, descricao: null };
+        var tamanhosCriados = [tamanhoNull];
+
+        for (var h = 0; h < tamanhos.length; h++) {
+            var tamanho = tamanhos[h];
+            tamanhosCriados.push(tamanho);
+        }
+
+        return tamanhosCriados;
+    }
 }
